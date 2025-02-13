d744b2ab29b11802dc42614fb5f7471667f35a02
Eric Huber <122307703+HuberEric105@users.noreply.github.com>
2025-02-13 12:57:38 -0600

    Add files via upload

---
+++ C-Unit-1-Project/Unit1Project/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Unit1Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.processBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.firstTB = new System.Windows.Forms.TextBox();
+            this.binaryTB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // processBtn
+            // 
+            this.processBtn.Location = new System.Drawing.Point(252, 140);
+            this.processBtn.Name = "processBtn";
+            this.processBtn.Size = new System.Drawing.Size(75, 23);
+            this.processBtn.TabIndex = 0;
+            this.processBtn.Text = "Process";
+            this.processBtn.UseVisualStyleBackColor = true;
+            this.processBtn.Click += new System.EventHandler(this.processBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(333, 140);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 1;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.exitBtn.Location = new System.Drawing.Point(414, 140);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(75, 23);
+            this.exitBtn.TabIndex = 2;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(356, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Binary Code:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 87);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "This application will take your name and return the binary code for each letter.";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Please enter your name";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // firstTB
+            // 
+            this.firstTB.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.firstTB.Location = new System.Drawing.Point(51, 140);
+            this.firstTB.Name = "firstTB";
+            this.firstTB.Size = new System.Drawing.Size(100, 20);
+            this.firstTB.TabIndex = 0;
+            this.firstTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // binaryTB
+            // 
+            this.binaryTB.Location = new System.Drawing.Point(312, 62);
+            this.binaryTB.Multiline = true;
+            this.binaryTB.Name = "binaryTB";
+            this.binaryTB.ReadOnly = true;
+            this.binaryTB.Size = new System.Drawing.Size(150, 50);
+            this.binaryTB.TabIndex = 8;
+            this.binaryTB.TabStop = false;
+            this.binaryTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.processBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.exitBtn;
+            this.ClientSize = new System.Drawing.Size(530, 205);
+            this.Controls.Add(this.binaryTB);
+            this.Controls.Add(this.firstTB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.processBtn);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Name Processor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button processBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox firstTB;
+        private System.Windows.Forms.TextBox binaryTB;
+    }
+}
+
---
+++ C-Unit-1-Project/Unit1Project/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unit1Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            firstTB.Focus();
+        }
+        
+        private void processBtn_Click(object sender, EventArgs e)
+        {
+            // Get the input string, convert it to upper case
+            string input = firstTB.Text.ToUpper();
+            string[] binaryValues = new string[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch (input[i])
+                {
+                    case 'A':
+                        binaryValues[i] = "00001";
+                        break;
+                    case 'B':
+                        binaryValues[i] = "00010";
+                        break;
+                    case 'C':
+                        binaryValues[i] = "00011";
+                        break;
+                    case 'D':
+                        binaryValues[i] = "00100";
+                        break;
+                    case 'E':
+                        binaryValues[i] = "00101";
+                        break;
+                    case 'F':
+                        binaryValues[i] = "00110";
+                        break;
+                    case 'G':
+                        binaryValues[i] = "00111";
+                        break;
+                    case 'H':
+                        binaryValues[i] = "01000";
+                        break;
+                    case 'I':
+                        binaryValues[i] = "01001";
+                        break;
+                    case 'J':
+                        binaryValues[i] = "01010";
+                        break;
+                    case 'K':
+                        binaryValues[i] = "01011";
+                        break;
+                    case 'L':
+                        binaryValues[i] = "01100";
+                        break;
+                    case 'M':
+                        binaryValues[i] = "01101";
+                        break;
+                    case 'N':
+                        binaryValues[i] = "01110";
+                        break;
+                    case 'O':
+                        binaryValues[i] = "01111";
+                        break;
+                    case 'P':
+                        binaryValues[i] = "10000";
+                        break;
+                    case 'Q':
+                        binaryValues[i] = "10001";
+                        break;
+                    case 'R':
+                        binaryValues[i] = "10010";
+                        break;
+                    case 'S':
+                        binaryValues[i] = "10011";
+                        break;
+                    case 'T':
+                        binaryValues[i] = "10100";
+                        break;
+                    case 'U':
+                        binaryValues[i] = "10101";
+                        break;
+                    case 'V':
+                        binaryValues[i] = "10110";
+                        break;
+                    case 'W':
+                        binaryValues[i] = "10111";
+                        break;
+                    case 'X':
+                        binaryValues[i] = "11000";
+                        break;
+                    case 'Y':
+                        binaryValues[i] = "11001";
+                        break;
+                    case 'Z':
+                        binaryValues[i] = "11010";
+                        break;
+                    default:
+                        binaryValues[i] = "";
+                        break;
+                }
+            }
+            binaryTB.Text = string.Join(" ", binaryValues);
+            firstTB.Text = "";
+            firstTB.Focus();
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            binaryTB.Text = "";
+            firstTB.Focus();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+    }
+}
